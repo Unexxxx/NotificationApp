6836a30f08325a7a61029ee92d133bce983ea157
Toby Rodin Roman <toby.roman@decenternet.net>
2021-03-06 10:09:45 +0800

    Added Function

--- app/src/main/java/com/unex/notificationapp/MainActivity.java
+++ app/src/main/java/com/unex/notificationapp/MainActivity.java
@@ -2,22 +2,28 @@ package com.unex.notificationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.unex.notificationapp.NotifyPage;
 import com.unex.notificationapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView lvActivities;
     ArrayAdapter<String> activitiesArrayAdapter;
-    String[] mStringArray = {"ONLINE CHAPTER MEETING\nMONDAY TO SATURDAY\n12nn","MONTHLY CHAPTER MEETING\nARAW-ARAW\n8pm", "SELF-AWARENESS SEMINAR\nFEBRUARY 27, 2021\n8AM", "HOME DECOR\nDIKO ALAM\nsecret", "WELCOME BEACH\nWELCOMING NANAY MO\ndiko alam"};
-    Button btnAdd;
+    String[] mStringArray = {};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +33,54 @@ public class MainActivity extends AppCompatActivity {
         lvActivities = findViewById(R.id.lv_activities);
         activitiesArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mStringArray);
         lvActivities.setAdapter(activitiesArrayAdapter);
+
+        SharedPreferences notifSharedPref = getSharedPreferences("notification", Context.MODE_PRIVATE);
+        String notificationData = notifSharedPref.getString("notifData", null);
+        Gson gson = new Gson();
+        List<String> data = new ArrayList<>();
+        if(notificationData != null){
+            data = gson.fromJson(notificationData, new TypeToken<List<String>>(){}.getType());
+        }
+        mStringArray = new String[data.size()];
+        mStringArray = (String[]) data.toArray(mStringArray);
+        activitiesArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mStringArray);
+        lvActivities.setAdapter(activitiesArrayAdapter);
+        lvActivities.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                SharedPreferences aklatSharedPref = getSharedPreferences("notification", Context.MODE_PRIVATE);
+                String aklatData = aklatSharedPref.getString("notifData", null);
+                Gson gson = new Gson();
+                List<String> data = new ArrayList<>();
+                if(aklatData != null){
+                    data = gson.fromJson(aklatData, new TypeToken<List<String>>(){}.getType());
+                }
+
+                //remove data from listview
+                data.remove(mStringArray[i]);
+                activitiesArrayAdapter.notifyDataSetChanged();
+
+                mStringArray = new String[data.size()];
+                mStringArray = (String[]) data.toArray(mStringArray);
+                activitiesArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, mStringArray);
+                lvActivities.setAdapter(activitiesArrayAdapter);
+                saveToSharedPref(data);
+                return false;
+            }
+        });
     }
 
     public void onClickAddNotif(View v){
         Intent intent = new Intent(this, NotifyPage.class);
         startActivity(intent);
+    }
+
+    private void saveToSharedPref(List<String> wholeData){
+        Gson gson = new Gson();
+        String wholeDataString = gson.toJson(wholeData);
+        SharedPreferences notifSharedPref = getSharedPreferences("notification", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = notifSharedPref.edit();
+        editor.putString("notifData", wholeDataString);
+        editor.apply();
     }
 }
--- app/src/main/java/com/unex/notificationapp/NotifyPage.java
+++ app/src/main/java/com/unex/notificationapp/NotifyPage.java
@@ -3,22 +3,34 @@ package com.unex.notificationapp;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.unex.notificationapp.R;
 import com.vivekkaushik.datepicker.DatePickerTimeline;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class NotifyPage extends AppCompatActivity {
 
+    int currentYear, currentMonth, currentDay;
     private TimePicker tpAlarmTimePicker;
+    DatePickerTimeline datePickerTimeline;
+    EditText etLabel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +41,46 @@ public class NotifyPage extends AppCompatActivity {
         getSupportActionBar().setCustomView(R.layout.custom_action_bar);
 //        View view = getSupportActionBar().getCustomView();
 
-        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
-        int currentDay = Calendar.getInstance().get(Calendar.DATE);
+        currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        currentMonth = Calendar.getInstance().get(Calendar.MONTH);
+        currentDay = Calendar.getInstance().get(Calendar.DATE);
 
-        DatePickerTimeline datePickerTimeline = findViewById(R.id.dateTimeline);
+        tpAlarmTimePicker = findViewById(R.id.tp_alarm_time_Picker);
+        datePickerTimeline = findViewById(R.id.date_picker);
+        etLabel = findViewById(R.id.et_label);
         datePickerTimeline.setInitialDate(currentYear, currentMonth, currentDay);
+
     }
 
     public void onClickBack(View v){
         finish();
     }
     public void onClickSave(View v){
-        Toast.makeText(this, "Saved!!!", Toast.LENGTH_SHORT).show();
+
+        String date = String.valueOf(currentYear + currentMonth + currentDay);
+        String time = tpAlarmTimePicker.toString();
+        String label = etLabel.getText().toString();
+        saveToSharedPref(label + " " + date + " " + time);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+    private void saveToSharedPref(String activityData){
+        Gson gson = new Gson();
+
+        SharedPreferences aklatSharedPref = getSharedPreferences("notification", Context.MODE_PRIVATE);
+        String aklatData = aklatSharedPref.getString("notifData", null);
+
+        ArrayList<String> data = new ArrayList<>();
+        if(aklatData != null){
+            data = gson.fromJson(aklatData, new TypeToken<List<String>>(){}.getType());
+        }
+        data.add(activityData);
+
+        String dataString = gson.toJson(data);
+        SharedPreferences.Editor editor = aklatSharedPref.edit();
+        editor.putString("notifData", dataString);
+        editor.commit();
     }
 
 }
